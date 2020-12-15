ba241acb6409cb98502b032f1f9b2176577fdad0
Dugersuren <dugersuren@gmail.com>
2020-12-15 17:46:02 +0800

    sss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 const data = {
   data: [
     { id: 1, text: 'Task #1', start_date: '2020-02-12', duration: 3, progress: 0.6 },
-    { id: 2, text: 'Task #2', start_date: '2020-02-16', duration: 3, progress: 0.4 }
+    { id: 2, text: 'Task #2', start_date: '2020-02-16', duration: 3, progress: 0.4 },
+    { id: 3, text: 'Task #3', start_date: '2020-02-18', duration: 3, progress: 0.4 }
   ],
   links: [
     { id: 1, source: 1, target: 2, type: '0' }
